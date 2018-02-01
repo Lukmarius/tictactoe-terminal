5bd9107a32b59eb06845efe1f26c551ee18ec3cb
Mariusz J <mariusz.jaskola.92@gmail.com>
2018-02-01 19:59:29 +0100

    TicTacToe 2.0 -poziomy trudnosci,  poprawiony styl funkcji

--- TicTacToe.py
+++ TicTacToe.py
@@ -67,7 +67,7 @@ keyfields = [[2, 7, 2],
 sep = "        "
 
 
-def refresh_board():
+def refresh_board(fields):
     clear()  # clearing terminal
     board = ("\n\n" + sep*2 + sep.join(fields[6:9]) + "\n\n\n\n" +
                       sep*2 + sep.join(fields[3:6]) + "\n\n\n\n" +
@@ -75,7 +75,7 @@ def refresh_board():
     return board
 
 
-def schema():
+def schema(a, b, c):
     schema = ("Schema of numerical keyboard \n" +
               sep.join(a) + "\n\n" +
               sep.join(b) + "\n\n" +
@@ -96,7 +96,7 @@ def ask_player(xo):
     return num
 
 
-def put(xo, num):  # Function of putting "X" or "O" by users
+def put(xo, num, fields):  # Function of putting "X" or "O" by users
     if fields[num - 1] == "-":
         fields[num - 1] = xo
         if fields.count("-") == 0:
@@ -204,7 +204,7 @@ def target_f(keyfields, fields, xo):
         sys.exit
 
 
-def AI_put(xo, level):  # Function of putting "X" or "O" by computer
+def AI_put(xo, level, fields):  # Function of putting "X" or "O" by computer
 
     if fields.count(xo) == 0:  # if there is no AI symbols on board
         if level == 'y':  # if hard level is choosen
@@ -244,34 +244,34 @@ def AI_put(xo, level):  # Function of putting "X" or "O" by computer
 
 def multiplayer():
     state = 0
-    print(refresh_board())
+    print(refresh_board(fields))
     while state != 2:
         num = ask_player('X')
-        state = put('X', num)
+        state = put('X', num, fields)
         if state == 2:
             break
 
         while state == 1:
             num = ask_player('X')
-            state = put('X', num)
+            state = put('X', num, fields)
             if state == 2:
                 break
 
         num = ask_player('O')
-        state = put("O", num)
+        state = put("O", num, fields)
         if state == 2:
             break
 
         while state == 1:
             num = ask_player('O')
-            state = put("O", num)
+            state = put("O", num, fields)
             if state == 2:
                 break
 
 # Singleplayer mode VS Computer:
 
 
-def singleplayer():
+def singleplayer(level):
     state = 0
     aistate = 0
     while True:
@@ -284,44 +284,44 @@ def singleplayer():
                 print(
                     "PLease make sure to enter a number! 1 to start first, 2 to start second")
         if menu2 == 1:  # User starts as first
-            print(refresh_board())
+            print(refresh_board(fields))
             while state != 2:
                 # User moves:
                 num = ask_player('X')
-                state = put("X", num)
+                state = put("X", num, fields)
                 if state == 2:
                     break
 
                 while state == 1:
                     num = ask_player('X')
-                    state = put("X", num)
+                    state = put("X", num, fields)
                     if state == 2:
                         break
 
                 # Computer moves:
                 sleep(2)
-                aistate = AI_put("O", level)
+                aistate = AI_put("O", level, fields)
                 if aistate == 2:
                     break
 
         elif menu2 == 2:  # Computer starts as first
-            print(refresh_board())
+            print(refresh_board(fields))
             while state != 2 or aistate != 2:
                 # Computer moves:
                 sleep(2)
-                aistate = AI_put("X", level)
+                aistate = AI_put("X", level, fields)
                 if aistate == 2:
                     break
 
                 # User moves:
                 num = ask_player('O')
-                state = put("O", num)
+                state = put("O", num, fields)
                 if state == 2:
                     break
 
                 while state == 1:
                     num = ask_player('O')
-                    state = put("O", num)
+                    state = put("O", num, fields)
                     if state == 2:
                         break
 
@@ -349,7 +349,7 @@ while True:
             level = input('Please enter the letter y or n!\n').lower()
             if level == 'y' or level == 'n':
                 break
-        singleplayer()
+        singleplayer(level)
         break
 
     elif choice == "r":  # show rules
